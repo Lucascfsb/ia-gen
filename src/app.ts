e92bd9d806fb7582dc3800519f2d7442430ba103
Lucas Castro <lucascfsb@gmail.com>
2026-01-27 12:51:43 -0300

    adicionando embedding

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,31 @@
-import express from "express";
-import {generataProducts} from "./gemini";
+import express from 'express';
+import { embedProducts, generateEmbedding, generateProducts } from "./openai";
+import { todosProdutos } from "./database";
 
 const app = express();
+
 app.use(express.json());
 
-app.post("/generate", async (req, res) => {
+app.post('/generate', async (req, res) => {
   try {
-    const produtos = await generataProducts(req.body.prompt);
-    res.json(produtos);
-  } catch (error) {
-      console.error("Erro interno:", error);
-      res.status(500).json({ error: "Erro interno do servidor" });
-    }
+    const products = await generateProducts(req.body.message);
+    res.json(products);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/embeddings', async (req, res) => {
+  await embedProducts();
+  console.log(todosProdutos());
+  res.status(201).end();
+});
+
+app.post('/embedding', async (req, res) => {
+  const { input } = req.body;
+  await generateEmbedding(input);
+  res.status(201).end();
 });
 
 export { app };
-
-
-// app.post("/generate", async (req, res) => {
-//   try {
-//     const response = await client.models.generateContent({
-//       model: "gemini-2.5-flash-lite",
-//       contents: [
-//         {
-//           text: `Instruções: Liste três produtos que atendam a necessidade do usuário. ${req.body.prompt}`,
-//         },
-//       ],
-//       config: {
-//         responseMimeType: "application/json",
-//       },
-//     });
-
-//     //console.log("Resposta da API:", response.text);
-
-//     const output = JSON.parse(response.text || "[]");
-
-//     const transformedOutput = {
-//       produtos: Array.isArray(output)
-//         ? output.map((item: any) => item.produto ?? item.nome ?? item)
-//         : [],
-//     };
-
-//     //console.log("Resposta transformada:", transformedOutput);
-
-//     const result = schema.safeParse(transformedOutput);
-
-//     if (!result.success) {
-//       console.error("Erro de validação do Zod:", result.error);
-//       return res.status(500).json({ error: "Formato de resposta inválido", details: result.error });
-//     }
-
-//     res.json(result.data.produtos);
-//   } catch (error) {
-//     console.error("Erro interno:", error);
-//     res.status(500).json({ error: "Erro interno do servidor" });
-//   }
-// });
---
+++ src/openai.ts
@@ -0,0 +1,124 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import OpenAI from 'openai';
+import { zodResponseFormat } from 'openai/helpers/zod';
+import { ChatCompletionMessageParam, ChatCompletionTool } from "openai/resources";
+import { z } from 'zod';
+import { produtosEmEstoque, produtosEmFalta, setarEmbedding, todosProdutos } from "./database";
+
+const schema = z.object({
+  produtos: z.array(z.string()),
+});
+
+const client = new OpenAI({
+  apiKey: process.env.OPEN_AI_API_KEY,
+});
+
+const tools: ChatCompletionTool[] = [
+  {
+    type: 'function',
+    function: {
+      name: 'produtos_em_estoque',
+      description: 'Lista os produtos que estão em estoque.',
+      parameters: {
+        type: 'object',
+        properties: {},
+        additionalProperties: false,
+      },
+      strict: true,
+    },
+  },
+  {
+    type: 'function',
+    function: {
+      name: 'produtos_em_falta',
+      description: 'Lista os produtos que estão em falta.',
+      parameters: {
+        type: 'object',
+        properties: {},
+        additionalProperties: false,
+      },
+      strict: true,
+    },
+  },
+];
+
+const generateCompletion = async (messages: ChatCompletionMessageParam[], format: any): Promise<any> => {
+  const completion = await client.chat.completions.parse({
+    model: 'gpt-4o-mini',
+    max_tokens: 100,
+    response_format: format,
+    tools,
+    messages,
+  });
+
+  if (completion.choices[0].message.refusal) {
+    throw new Error('Refusal');
+  }
+
+  const { tool_calls } = completion.choices[0].message;
+  if (tool_calls) {
+    const [tool_call] = tool_calls;
+    const toolsMap: { [key: string]: (args: any) => string[] } = {
+      produtos_em_estoque: produtosEmEstoque,
+      produtos_em_falta: produtosEmFalta,
+    }
+    const functionToCall = toolsMap[tool_call.function.name];
+    if (!functionToCall) {
+      throw new Error('Function not found');
+    }
+    const result = functionToCall(tool_call.function.parsed_arguments);
+    messages.push(completion.choices[0].message);
+    messages.push({
+      role: 'tool',
+      tool_call_id: tool_call.id,
+      content: result.toString(),
+    })
+    const completionWithToolResult = await generateCompletion(messages, zodResponseFormat(schema, 'produtos_schema'));
+    return completionWithToolResult;
+  }
+
+  return completion;
+}
+
+export const generateProducts = async (message: string) => {
+  const messages: ChatCompletionMessageParam[] = [
+    {
+      role: 'developer',
+      content:
+        'Liste no máximo três produtos que atendam a necessidade do usuário. Considere apenas os produtos em estoque.',
+    },
+    {
+      role: 'user',
+      content: message,
+    },
+  ];
+
+  const completion = await generateCompletion(messages, zodResponseFormat(schema, 'produtos_schema'));
+
+  return completion.choices[0].message.parsed;
+};
+
+export const generateEmbedding = async (input: string) => {
+  try {
+    const response = await client.embeddings.create({
+      input,
+      model: 'text-embedding-3-small',
+      encoding_format:'float',
+    });
+
+    return response.data[0].embedding ?? null;
+  } catch (error) {
+    return null;
+  }
+}
+
+export const embedProducts = async () => {
+  const produtos = todosProdutos();
+
+  await Promise.allSettled(produtos.map(async (p, index) => {
+    const embedding = await generateEmbedding(`${p.nome}: ${p.descricao}`);
+    if (!embedding) return;
+    setarEmbedding(index, embedding);
+  }))
+}
